82b64936a7f36bdb746af6d15282e355635055a6
mugisho <mugishompozi@gmail.com>
2023-09-27 12:13:24 +0200

    get it to compile

---
+++ Assignment2/tst/getA_test.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include "inst-decoder.h"
+// Assuming the relevant headers are included here
+
+TEST(InstructionDecoderTest, GetAShouldReturnCorrectValue) {
+    InstructionDecoder decoder;
+
+    // Test case 1: Value with A in bits 20-16 is 0x00160000
+    decoder.setInstructionWord(0x00160000);
+    EXPECT_EQ(decoder.getA(), 0x0016);
+
+    // Test case 2: Value with A in bits 20-16 is 0x001F0000
+    decoder.setInstructionWord(0x001F0000);
+    EXPECT_EQ(decoder.getA(), 0x001F);
+
+    // Add more test cases as needed
+}
