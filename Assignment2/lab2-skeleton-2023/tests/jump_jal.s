8458edd4f83130f3ccf69fad3c6a22a818a04670
mugisho <mugishompozi@gmail.com>
2023-11-13 01:04:40 +0100

    better jump_jal unit test

--- Assignment2/lab2-skeleton-2023/tests/jump_jal.s
+++ Assignment2/lab2-skeleton-2023/tests/jump_jal.s
@@ -6,26 +6,13 @@
 _start:
     l.add r1, r1, r2
     l.add r1, r1, r3
-    l.j target_label_j
+    l.j target_label
     l.add r1, r1, r4
-
-target_label_j:
     l.add r1, r1, r5
-    l.nop
-    l.nop
-    l.nop
-    l.nop
-    l.nop
-    .word 0x40ffccff
 
-    l.add r1, r1, r2
-    l.add r1, r1, r3
-    l.jal target_label_jal
-    l.add r1, r1, r4
-
-target_label_jal:
-    l.add r1, r1, r5
-    l.addi r31, r0, 40  # Assuming link register is R31
+target_label:
+    l.jal link_target_label
+    l.addi r9, r0, 40  # Assuming link register is R9
     l.nop
     l.nop
     l.nop
@@ -33,3 +20,12 @@ target_label_jal:
     l.nop
     .word 0x40ffccff
     .size _start, .-_start
+
+link_target_label:
+    l.nop
+    l.nop
+    l.nop
+    l.nop
+    l.nop
+    l.nop
+    .word 0x40ffccff
