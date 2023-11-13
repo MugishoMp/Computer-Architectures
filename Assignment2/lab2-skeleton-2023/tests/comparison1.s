f68553c255ec40fe605978f2c59f0a13bb5b5840
mugisho <mugishompozi@gmail.com>
2023-11-13 12:05:37 +0100

    tests

--- Assignment2/lab2-skeleton-2023/tests/comparison1.s
+++ Assignment2/lab2-skeleton-2023/tests/comparison1.s
@@ -3,6 +3,14 @@
 .globl _start
 .type _start, @function
 
+
+end_program:
+    l.addi r4, r0, 66
+    l.nop
+    l.nop
+    .word 0x40ffccff
+    .size _start, .-_start
+
 _start:
     l.addi r2, r0, 1
     l.addi r3, r0, 5
@@ -12,10 +20,3 @@ _start:
     l.sfeq r2, r3
     l.bf end_program
     l.nop
-
-end_program:
-    l.addi r4, r0, 66
-    l.nop
-    l.nop
-    .word 0x40ffccff
-    .size _start, .-_start
