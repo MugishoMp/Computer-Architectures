43577ffe44bdd586a7062f12d5d6ef8ecc9a090e
mugisho <mugishompozi@gmail.com>
2023-11-13 04:48:45 +0100

    test

--- Assignment2/lab2-skeleton-2023/tests/arithmetic_test.s
+++ Assignment2/lab2-skeleton-2023/tests/arithmetic_test.s
@@ -1,3 +1,10 @@
+# 10 + 3 = 13
+# 7 - 2 = 5
+# 1101 & 0101 = 0101
+# 1101 | 0101 = 1101
+# 1101 << 5 = 1 1010 0000
+# 0101 >> 5 = 0
+
 .text
 .align 4
 .globl _start
@@ -6,10 +13,10 @@
 _start:
     l.add r1, r2, r3
     l.sub r2, r4, r5
-    l.and r3, r1, r2
-    l.or  r4, r1, r2
-    l.sll r5, r4, r3
-    l.srl r6, r2, r3
+    l.and r3, r1, r2 
+    l.or  r4, r1, r2 
+    l.sll r5, r4, r3 
+    l.srl r6, r2, r3 
     l.nop
     l.nop
     l.nop
