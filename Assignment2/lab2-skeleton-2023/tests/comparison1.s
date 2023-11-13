972d03e21d04c7e09a14a08cdb3eeaf759de37a3
mugisho <mugishompozi@gmail.com>
2023-11-13 11:59:54 +0100

    utest

--- Assignment2/lab2-skeleton-2023/tests/comparison1.s
+++ Assignment2/lab2-skeleton-2023/tests/comparison1.s
@@ -6,10 +6,10 @@
 _start:
     l.addi r2, r0, 1
     l.addi r3, r0, 5
-    l.sfeq r1, r2, r3
+    l.sfeq r1, r2
     l.bf no_branch_1
     l.addi r3, r0, 1
-    l.sfeq r1, r2, r3
+    l.sfeq r1, r2
     l.bf end_program
     l.nop
 
